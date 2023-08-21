e9a13f396c3f2813db23c85bc3f233411c51afee
skimmboard <shopingmadness@gmail.com>
2023-08-21 07:20:10 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,435 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-przedpokj",
+      "name": "Przedpokój",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.0694958374885104,
+        "pitch": 0.052035000932615816,
+        "fov": 1.4193224075792448
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.005205725763868685,
+          "pitch": 0.4235495032683705,
+          "rotation": 0,
+          "target": "2--salon-cz-1"
+        },
+        {
+          "yaw": 2.192371796668632,
+          "pitch": 0.759780200722501,
+          "rotation": 0,
+          "target": "4-przedpokj-cz-2"
+        },
+        {
+          "yaw": 2.7271228653581048,
+          "pitch": 0.46084433100265976,
+          "rotation": 0,
+          "target": "7-azienka"
+        },
+        {
+          "yaw": -2.164401094569513,
+          "pitch": 0.2370074356085805,
+          "rotation": 0,
+          "target": "8-klatka-schodowa"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.46781537470229395,
+          "pitch": 0.040708860220711074,
+          "title": "Szafa z przesuwnymi drzwiami",
+          "text": ""
+        },
+        {
+          "yaw": 2.2046671636748547,
+          "pitch": 0.019289039146570985,
+          "title": "WC",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-salon-cz-2",
+      "name": "Salon cz. 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.1044340691064587,
+        "pitch": 0.14773077406344726,
+        "fov": 1.4193224075792448
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6488162418577588,
+          "pitch": 0.5218142292771102,
+          "rotation": 5.497787143782138,
+          "target": "2--salon-cz-1"
+        },
+        {
+          "yaw": 2.9884392037517564,
+          "pitch": 0.1606144361475046,
+          "rotation": 0,
+          "target": "3-widok-z-balkonu"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3101140396715536,
+          "pitch": 0.16241167580636073,
+          "title": "Smart TV 50 cali",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2--salon-cz-1",
+      "name": " Salon cz. 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.3803401279125236,
+        "pitch": 0.1355455847639231,
+        "fov": 1.4193224075792448
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.24240468660025272,
+          "pitch": 0.5076834173335296,
+          "rotation": 0,
+          "target": "0-przedpokj"
+        },
+        {
+          "yaw": -1.991532614319329,
+          "pitch": 0.532140334524108,
+          "rotation": 12.566370614359176,
+          "target": "1-salon-cz-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-widok-z-balkonu",
+      "name": "Widok z balkonu",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.3618946632943576,
+        "pitch": -0.015212570466106357,
+        "fov": 1.4193224075792448
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10702508124805021,
+          "pitch": 0.3131665280856666,
+          "rotation": 0,
+          "target": "2--salon-cz-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3845245257666488,
+          "pitch": -0.06124452045710527,
+          "title": "Rynek w Gliwicach",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-przedpokj-cz-2",
+      "name": "Przedpokój cz. 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.021265640251701967,
+          "pitch": 0.5712596248098514,
+          "rotation": 0,
+          "target": "5-kuchnia"
+        },
+        {
+          "yaw": -0.8105187793778263,
+          "pitch": 0.6347334714759931,
+          "rotation": 0,
+          "target": "6-sypialnia"
+        },
+        {
+          "yaw": 2.460514633326226,
+          "pitch": 0.49042099603035005,
+          "rotation": 0,
+          "target": "7-azienka"
+        },
+        {
+          "yaw": -2.690089358459259,
+          "pitch": 0.7449587342182085,
+          "rotation": 0,
+          "target": "0-przedpokj"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6005524167982141,
+          "pitch": 0.08552977141234663,
+          "title": "WC",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "5-kuchnia",
+      "name": "Kuchnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.2941953531454935,
+        "pitch": 0.1359367876001798,
+        "fov": 1.4193224075792448
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0861268108793904,
+          "pitch": 0.5693510559392898,
+          "rotation": 0,
+          "target": "4-przedpokj-cz-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sypialnia",
+      "name": "Sypialnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9391844480623082,
+          "pitch": 0.4169025474399124,
+          "rotation": 0,
+          "target": "4-przedpokj-cz-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-azienka",
+      "name": "Łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.2680610347217005,
+        "pitch": 0.22251483397960925,
+        "fov": 1.4193224075792448
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.915739714563532,
+          "pitch": 0.7067094864694656,
+          "rotation": 4.71238898038469,
+          "target": "0-przedpokj"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-klatka-schodowa",
+      "name": "Klatka schodowa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.004759988869086129,
+        "pitch": 0.19904338071459904,
+        "fov": 1.4193224075792448
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8101053589334226,
+          "pitch": 0.29844583486457665,
+          "rotation": 0,
+          "target": "0-przedpokj"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mieszkanie Gliwice, Pszczyńska",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
